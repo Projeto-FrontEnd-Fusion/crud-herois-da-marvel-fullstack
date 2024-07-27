44f446af216120a729e3216892853fa4a98c1fc4
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 07:55:09 -0300

    DELETE : delete default integrations

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 
 import './App.css'
-import useSWR from 'swr'
-import { fetcherMarvel, urlMarvelcharacters } from './api'
-
-
 function App() {
 
-  const {data} = useSWR(urlMarvelcharacters, fetcherMarvel)
-console.log(data)
 
   return (
     <>
       <main>
         <span>Bem vindo ao Desafio Fullstack </span>
-        <form action="">
-          <legend>Adicionar novos Herois</legend>
-        </form>
-
-      </main>
+          </main>
     </>
   )
 }
--- src/apihash.ts
+++
@@ -1,7 +0,0 @@
-import { MD5 } from "crypto-js";
-
-
-
-export const ts = new Date().getTime().toString();
-export const dataHash = ts + import.meta.env.VITE_PRIVATE_KEY + import.meta.env.VITE_PUBLIC_KEY;
-export const hash = MD5(dataHash).toString()
