a3a75b25e268421ede22596f9abc22fc83cda39a
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-27 07:48:17 -0300

    FIX : defalts modify

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 
 import './App.css'
+import useSWR from 'swr'
+import { fetcherMarvel, urlMarvelcharacters } from './api'
+
 
 function App() {
 
+  const {data} = useSWR(urlMarvelcharacters, fetcherMarvel)
+console.log(data)
+
   return (
-<>
-<main>
-  <span>Bem vindo ao Desafio Fullstack </span>
-</main>
-</>
+    <>
+      <main>
+        <span>Bem vindo ao Desafio Fullstack </span>
+        <form action="">
+          <legend>Adicionar novos Herois</legend>
+        </form>
+
+      </main>
+    </>
   )
 }
 
